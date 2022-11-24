6f6e10bc34c126ad7d6ab675c3f2fec0b01c83d7
alvarogallegomartinez <alvaro.gallego.martinez@alumnos.upm.es>
2022-11-24 10:52:43 +0100

    Traspaso a IntelliJ y casos de prueba 2 y 3

--- src/test/java/com/practica/cajanegra/test_constructor.java
+++ src/test/java/com/practica/cajanegra/test_constructor.java
@@ -10,9 +10,20 @@ public class test_constructor {
 	
 	@Test
 	public void testConstructorCP1() {
-		prueba = new BinaryTree<String>("100");
+		prueba = new BinaryTree<>("100");
 		Assert.assertEquals(prueba.getRoot().getContent(), "100");
-		
     }
+
+	@Test
+	public void testConstructorCP2() {
+		prueba = new BinaryTree<>("Prueba");
+		Assert.assertEquals(prueba.getRoot().getContent(), "Prueba");
+	}
+
+	@Test
+	public void testConstructorCP3() {
+		prueba = new BinaryTree<>("Prueba100");
+		Assert.assertEquals(prueba.getRoot().getContent(), "Prueba100");
+	}
 	
 }
