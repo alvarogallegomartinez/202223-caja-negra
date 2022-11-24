ede4232ef0bdca9aeb9494d30bda2c19542bf5b1
dmarguelles <d.marguelles@alumnos.upm.es>
2022-11-24 10:23:21 +0100

    Primer test de constructor

--- src/test/java/com/practica/cajanegra/test_constructor.java
+++ src/test/java/com/practica/cajanegra/test_constructor.java
@@ -1,5 +1,18 @@
 package com.practica.cajanegra;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.binarytree.BinaryTree;
+
 public class test_constructor {
+	BinaryTree<String> prueba;
+	
+	@Test
+	public void testConstructorCP1() {
+		prueba = new BinaryTree<String>("100");
+		Assert.assertEquals(prueba.getRoot().getContent(), "100");
+		
+    }
 	
 }
